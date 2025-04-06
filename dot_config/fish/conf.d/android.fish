b6739081c0e2646786acaca4286e9e79c445f14c
Goooler <wangzongler@gmail.com>
2025-04-06 14:50:51 +0800

    Add android.fish and miscs.fish

---
+++ dot_config/fish/conf.d/android.fish
@@ -0,0 +1,25 @@
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+set -gx ANDROID_SDK $HOME/Library/Android/sdk
+set -gx ANDROID_SDK_ROOT $ANDROID_SDK
+set -gx ANDROID_HOME $ANDROID_SDK
+set -gx ANDROID_SDK_CMD_TOOLS $ANDROID_SDK/cmdline-tools/latest/bin
+set -gx ANDROID_SDK_PLATFORM_TOOLS $ANDROID_SDK/platform-tools
+set -gx ANDROID_SDK_PROGUARD $ANDROID_SDK/tools/proguard/bin
+
+set -gx PATH /usr/local/sbin $PATH
+set -gx PATH $ANDROID_SDK $PATH
+set -gx PATH $ANDROID_SDK_ROOT $PATH
+set -gx PATH $ANDROID_HOME $PATH
+set -gx PATH $ANDROID_SDK_CMD_TOOLS $PATH
+set -gx PATH $ANDROID_SDK_PLATFORM_TOOLS $PATH
+set -gx PATH $ANDROID_SDK_PROGUARD $PATH
+
+
+function install-aab
+  set aab_path $argv[1]
+  set apks_path (string replace -r '\.[^.]*$' '.apks' $aab_path)
+
+  bundletool build-apks --bundle=$aab_path --output=$apks_path
+  bundletool install-apks --apks=$apks_path
+end
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,8 +3,6 @@ set fish_greeting
 
 set -gx EDITOR "subl -n -w"
 
-set -gx ANDROID_HOME $HOME/Library/Android/sdk
-
 alias ll="gls -vAhlF --color --group-directories-first"
 alias grep="grep --color=auto -n -I"
 
