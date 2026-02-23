746046fd37ab77fb3ebd7708848429f44f7756e3
Goooler <wangzongler@gmail.com>
2026-02-23 18:18:53 +0800

    Set ANDROID_NDK and ANDROID_NDK_HOME

--- fish/conf.d/android.fish
+++ fish/conf.d/android.fish
@@ -3,6 +3,8 @@ set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home
 set -gx ANDROID_SDK $HOME/Library/Android/sdk
 set -gx ANDROID_SDK_ROOT $ANDROID_SDK
 set -gx ANDROID_HOME $ANDROID_SDK
+set -gx ANDROID_NDK $ANDROID_SDK/ndk/29.0.14206865
+set -gx ANDROID_NDK_HOME $ANDROID_NDK
 set -gx ANDROID_SDK_CMD_TOOLS $ANDROID_SDK/cmdline-tools/latest/bin
 set -gx ANDROID_SDK_PLATFORM_TOOLS $ANDROID_SDK/platform-tools
 set -gx ANDROID_SDK_PROGUARD $ANDROID_SDK/tools/proguard/bin
@@ -11,6 +13,8 @@ set -gx PATH /usr/local/sbin $PATH
 set -gx PATH $ANDROID_SDK $PATH
 set -gx PATH $ANDROID_SDK_ROOT $PATH
 set -gx PATH $ANDROID_HOME $PATH
+set -gx PATH $ANDROID_NDK $PATH
+set -gx PATH $ANDROID_NDK_HOME $PATH
 set -gx PATH $ANDROID_SDK_CMD_TOOLS $PATH
 set -gx PATH $ANDROID_SDK_PLATFORM_TOOLS $PATH
 set -gx PATH $ANDROID_SDK_PROGUARD $PATH
