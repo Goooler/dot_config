1407c23853570919b44827b1bacade5beca85bed
Goooler <wangzongler@gmail.com>
2026-03-16 15:01:58 +0800

    Use starship back

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Disable the greeting.
 set fish_greeting
 
-# starship init fish | source
-oh-my-posh init fish --config ~/.config/omp/fish.dracula.omp.json | source
+starship init fish | source
+# oh-my-posh init fish --config ~/.config/omp/fish.dracula.omp.json | source
