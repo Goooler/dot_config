a711a098dcca56ec54449553391a2d1fa3819dda
Goooler <wangzongler@gmail.com>
2026-08-05 11:29:04 +0800

    Export GRAALVM_HOME

--- fish/conf.d/jdk.fish
+++ fish/conf.d/jdk.fish
@@ -4,3 +4,6 @@ function jdk
   set -gx JAVA_HOME $(/usr/libexec/java_home -v $argv[1]);
   java -version
 end
+
+set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-25.jdk/Contents/Home
+set -gx PATH $GRAALVM_HOME/bin $PATH
