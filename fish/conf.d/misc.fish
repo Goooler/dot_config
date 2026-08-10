70a427135fe80d7c1419eb2536e285155ef5f219
Goooler <wangzongler@gmail.com>
2026-08-10 20:05:15 +0800

    Use 16 threads for yp

--- fish/conf.d/misc.fish
+++ fish/conf.d/misc.fish
@@ -3,7 +3,7 @@ set -gx PATH $PATH $HOME/.local/bin
 alias grep="grep --color=auto -n -I"
 alias ll="gls -vAhlF --color --group-directories-first"
 alias sha256="shasum -a 256"
-alias yp="yt-dlp --concurrent-fragments 64 --cookies-from-browser chrome"
+alias yp="yt-dlp --concurrent-fragments 16 --cookies-from-browser chrome"
 
 
 function upfind
